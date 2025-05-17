c7af488e89f8c0aea1aa4ba80878338c9ef301a2
DESKTOP-5SKO262\Vimal Nirmal <vimalrock82@gmail.com>
2025-05-17 17:19:00 +0530

    Removed unwanted route file

--- src/routes/app.route.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { register, login } = require('../controllers/auth.controller');
-const router = express.Router();
-
-router.route('/login').get(login);
-
-module.exports = router;
